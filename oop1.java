55ab59dcbc191ff7e7b671e4e4c9236be347fadf
Jyoti Kumari <jk1076387@gmail.com>
2026-05-27 15:27:21 +0530

    Refactor oop1.java and enhance Student class usage
    Refactor oop1.java to improve structure and add examples of class usage.

--- oop1.java
+++ oop1.java
@@ -14,15 +14,83 @@ public class oop1 {
         //these all data that is roll name and marks in one single line then we can use class 
         //class is a named group of properties and functions(class start with capital letter)
         Student[] students=new Student[5];
-        students[0]=new Student(1,"satyarth",78.5f);
-        students[1]=new Student(2,"satyarth",78.5f);     
-        students[2]=new Student(3,"satyarth",78.5f);
-        students[3]=new Student(4,"satyarth",78.5f);
-        students[4]=new Student(5,"satyarth",78.5f);
-        for(int i=0;i<students.length;i++){
-            System.out.println(students[i].rollno+" "+students[i].name+" "+students[i].marks);
-        }
+        Student jyoti;
+        System.out.println(Arrays.toString(students));   //as the value  is not intialise so the value is null
+        //dynamically allots
+        Student students[0]=new Student();
+        //the first is  Student students[0] is the compile time(checking error) and the second is new Student() is the runtime(running time)
+        //when we create object of a cass it means that its instanciated
+        //new is dynamic memory allocation operator which is used to create object of a class and it returns the address of the object created
+        //Student() is constructor
+        //Student students[0] is reference variable which is used to refer the object created and it is used to access the properties and functions of the class
         
+        //Jyoti.rno=04;
+       //Jyoti.marks=90.8f;
+       //Jyoti.name="jyoti";
+    
+       //Jyoti.changeName("Shoe Lover");
+       //Jyoti.gretting();
 
+       System.out.println(Jyoti.rno);
+       System.out.println(Jyoti.name);
+       System.out.println(Jyoti.marks);
+        
+       Student random=new Student(Jyoti);
+       System.out.println(random.name);
+
+       StudentS random2=new StudentS();
+       System.out.println(random2.name);
+
+       Student one=new Student();
+       Student two=one;
+       one.name="Astuti";
+         System.out.println(two.name);
+
+    }
+}
+
+//create a class student which has 3 properties roll no, name and marks
+//for every single students
+public classs Student{
+    int rollno;
+    String name;
+    float marks=90;
+
+    //we need a way to add the values of the above
+    //properties object by object
+
+    //we need one word to access every object
+    //we need a function to change the name of the student
+    void changeName(String name){
+        this.name=name;  //this is used to refer the current object
+    }
+    void greeting(){
+        System.out.println("Hello my name is "+name);
+    }
+         
+    Student (){
+        this.rno=15;
+        this.name="Jyoti";
+        this.marks=89.1f;
+
+    }
+    StudentS(){
+        //this is how u call a constructor from another constructor
+        //internally: new Student(13,"Default Men",90.1f);
+        this(13,"Default Men",90.1f);   
+    }
+
+    //Student Arpit=new Student(17,"Arpit",90.8f);
+    //here this will replaced with arpit
+    Student(int rno, String name, float marks){
+        this.rno=rno;
+        this.name=name;
+        this.marks=marks;
+    }
+
+    Student(Student other){
+        this.name=other.name;
+        this.rno=other.rno;
+        this.marks=other.marks;
     }
 }
